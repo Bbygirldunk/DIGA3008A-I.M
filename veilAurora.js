2922c00f595fbdc979b1835c19090749b55cfb36
Bbygirldunk <2708798@students.wits.ac.za>
2025-05-21 00:51:32 +0200

    profile

---
+++ veilAurora.js
@@ -0,0 +1,48 @@
+const canvas = document.getElementById('veil-bg');
+const ctx = canvas.getContext('2d');
+
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
+
+window.addEventListener('resize', () => {
+  width = canvas.width = window.innerWidth;
+  height = canvas.height = window.innerHeight;
+});
+
+const layers = [
+  { color: '#fbc2eb', amp: 60, speed: 0.2, offset: 0 },
+  { color: '#fadadd', amp: 80, speed: 0.15, offset: 100 },
+  { color: '#ffaae1', amp: 100, speed: 0.1, offset: 200 },
+];
+
+function drawWave(color, amp, speed, offset, time) {
+  ctx.beginPath();
+  ctx.moveTo(0, height / 2);
+
+  for (let x = 0; x <= width; x++) {
+    let y = height / 2 +
+      Math.sin((x + offset + time * speed) * 0.01) * amp;
+    ctx.lineTo(x, y);
+  }
+
+  ctx.lineTo(width, height);
+  ctx.lineTo(0, height);
+  ctx.closePath();
+
+  ctx.fillStyle = color;
+  ctx.globalAlpha = 0.2;
+  ctx.fill();
+  ctx.globalAlpha = 1;
+}
+
+function animate(time) {
+  ctx.clearRect(0, 0, width, height);
+
+  for (let layer of layers) {
+    drawWave(layer.color, layer.amp, layer.speed, layer.offset, time);
+  }
+
+  requestAnimationFrame(animate);
+}
+
+animate(0);
